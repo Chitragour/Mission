62783593709eaf6f489b5181d8109dc86b6156b0
Chitragour <144784875+Chitragour@users.noreply.github.com>
2023-10-04 23:31:41 +0530

    Delete node_linux directory

--- node_linux/server.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express();
-app.use(express.static('public'));
-app.get('/', (req, res)=>{res.send("Welcome to the world of Chocolates ");});
-app.listen(8000);
-console.log("App is Listening on Port 8000");
